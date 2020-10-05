e08ddd0a009ce4f0cc14dc1f7d0ebd123b43358d
Christian Bianchi <whites11@gmail.com>
2020-10-05 14:20:58 +0200

    removed unused k8sclient from CAPI admission controllers

--- pkg/azuremachine/update.go
+++ pkg/azuremachine/update.go
@@ -2,26 +2,19 @@ package azuremachine
 
 import (
 	"context"
-	"fmt"
 	"strings"
 
-	infrastructurev1alpha2 "github.com/giantswarm/apiextensions/v2/pkg/apis/infrastructure/v1alpha2"
-	releasev1alpha1 "github.com/giantswarm/apiextensions/v2/pkg/apis/release/v1alpha1"
 	"github.com/giantswarm/apiextensions/v2/pkg/label"
-	"github.com/giantswarm/k8sclient/v4/pkg/k8sclient"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"k8s.io/api/admission/v1beta1"
-	restclient "k8s.io/client-go/rest"
 	capzv1alpha3 "sigs.k8s.io/cluster-api-provider-azure/api/v1alpha3"
-	capiv1alpha3 "sigs.k8s.io/cluster-api/api/v1alpha3"
 
 	"github.com/giantswarm/azure-admission-controller/pkg/validator"
 )
 
 type UpdateValidator struct {
-	k8sClient k8sclient.Interface
-	logger    micrologger.Logger
+	logger micrologger.Logger
 }
 
 type UpdateValidatorConfig struct {
@@ -29,32 +22,8 @@ type UpdateValidatorConfig struct {
 }
 
 func NewUpdateValidator(config UpdateValidatorConfig) (*UpdateValidator, error) {
-	var k8sClient k8sclient.Interface
-	{
-		restConfig, err := restclient.InClusterConfig()
-		if err != nil {
-			return nil, fmt.Errorf("failed to load key kubeconfig: %v", err)
-		}
-		c := k8sclient.ClientsConfig{
-			SchemeBuilder: k8sclient.SchemeBuilder{
-				capiv1alpha3.AddToScheme,
-				infrastructurev1alpha2.AddToScheme,
-				releasev1alpha1.AddToScheme,
-			},
-			Logger: config.Logger,
-
-			RestConfig: restConfig,
-		}
-
-		k8sClient, err = k8sclient.NewClients(c)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-	}
-
 	v := &UpdateValidator{
-		k8sClient: k8sClient,
-		logger:    config.Logger,
+		logger: config.Logger,
 	}
 
 	return v, nil
