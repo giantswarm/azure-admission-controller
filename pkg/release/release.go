08e4c1c55d322e6a68c4dff788c6859905a4b211
Nikola Prokopić <nikola@prokopic.rs>
2021-05-21 15:36:49 +0200

    Add CR filtering functionality

--- pkg/release/release.go
+++ pkg/release/release.go
@@ -4,14 +4,23 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"time"
 
 	releasev1alpha1 "github.com/giantswarm/apiextensions/v2/pkg/apis/release/v1alpha1"
 	"github.com/giantswarm/microerror"
+	gocache "github.com/patrickmn/go-cache"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func GetComponentVersionsFromRelease(ctx context.Context, ctrlClient client.Client, releaseVersion string) (map[string]string, error) {
+var (
+	// Cache of release components
+	// key: release version
+	// value: map "component name": "component version"
+	releaseComponentsCache = gocache.New(24*time.Hour, 24*time.Hour)
+)
+
+func getComponentVersionsFromReleaseFromAPI(ctx context.Context, ctrlClient client.Client, releaseVersion string) (map[string]string, error) {
 	// Release CR always starts with a "v".
 	if !strings.HasPrefix(releaseVersion, "v") {
 		releaseVersion = fmt.Sprintf("v%s", releaseVersion)
@@ -35,4 +44,45 @@ func GetComponentVersionsFromRelease(ctx context.Context, ctrlClient client.Clie
 	}
 
 	return ret, nil
+}
+
+func GetComponentVersionsFromRelease(ctx context.Context, ctrlClient client.Client, releaseVersion string) (map[string]string, error) {
+	// Release CR always starts with a "v".
+	if !strings.HasPrefix(releaseVersion, "v") {
+		releaseVersion = fmt.Sprintf("v%s", releaseVersion)
+	}
+	var err error
+
+	var components map[string]string
+	{
+		cachedComponents, ok := releaseComponentsCache.Get(releaseVersion)
+
+		if ok {
+			// Release components are found in cache
+			components = cachedComponents.(map[string]string)
+		} else {
+			components, err = getComponentVersionsFromReleaseFromAPI(ctx, ctrlClient, releaseVersion)
+			if err != nil {
+				return nil, microerror.Mask(err)
+			}
+
+			// Save in cache
+			releaseComponentsCache.Set(releaseVersion, components, gocache.DefaultExpiration)
+		}
+	}
+
+	return components, nil
+}
+
+func ContainsAzureOperator(ctx context.Context, ctrlClient client.Client, releaseVersion string) (bool, error) {
+	componentVersions, err := GetComponentVersionsFromRelease(ctx, ctrlClient, releaseVersion)
+	if err != nil {
+		return false, microerror.Mask(err)
+	}
+
+	if componentVersions["azure-operator"] == "" {
+		return false, nil
+	}
+
+	return true, nil
 }
