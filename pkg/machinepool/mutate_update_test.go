9d4f39907e01f8c2ce8c6047b24bb80ec47ee16c
Tuomas Mäkinen <tuomas@giantswarm.io>
2021-06-02 08:07:52 +0300

    Bump up apiextensions & k8sclient
    As a preparation for use of caching reader from latest `k8sclient`:
    
    * Upgrade `apiextensions/v2` -> `apiextensions/v3`.
    * Upgrade `k8sclient/v4` -> `k8sclient/v5`.

--- pkg/machinepool/mutate_update_test.go
+++ pkg/machinepool/mutate_update_test.go
@@ -5,7 +5,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/giantswarm/apiextensions/v2/pkg/apis/release/v1alpha1"
+	"github.com/giantswarm/apiextensions/v3/pkg/apis/release/v1alpha1"
 	"github.com/giantswarm/apiextensions/v3/pkg/annotation"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
