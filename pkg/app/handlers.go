afac23bc614893e8b06f1f25a9d50ba508cb38c2
Nikola Prokopić <nikola@prokopic.rs>
2021-07-16 10:51:43 +0200

    Update changelog and code docs

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -22,7 +22,7 @@ import (
 	"github.com/giantswarm/azure-admission-controller/pkg/validator"
 )
 
-// RegisterWebhookHandlers first creates all required webhook handlers and then it registers
+// RegisterWebhookHandlers first creates all required webhook handlers and then it adds
 // them to the specified HttpRequestHandler with appropriate paths.
 //
 // Examples:
