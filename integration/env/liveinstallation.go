bb4d8e7a244930c185e7097083fb83abebbe25ce
Nikola Prokopić <nikola@prokopic.rs>
2021-07-23 15:43:25 +0200

    Use default values instead of setting all environment vars

--- integration/env/liveinstallation.go
+++ integration/env/liveinstallation.go
@@ -4,7 +4,13 @@ package env
 
 import (
 	"fmt"
+	"net/url"
 	"os"
+	"path"
+	"strings"
+
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 const (
@@ -26,14 +32,40 @@ var (
 )
 
 func init() {
-	e2eBaseDomain = os.Getenv(envVarBaseDomain)
-	if e2eBaseDomain == "" {
-		panic(fmt.Sprintf("env var '%s' must not be empty", envVarBaseDomain))
-	}
+	var err error
 
 	kubeconfig = os.Getenv(EnvVarE2EKubeconfig)
 	if kubeconfig == "" {
-		panic(fmt.Sprintf("env var '%s' must not be empty", EnvVarE2EKubeconfig))
+		homeDir := os.Getenv("HOME")
+		if homeDir == "" {
+			panic(fmt.Sprintf("Env var '%s' must not be empty. Alternatively env var 'HOME' must be set and kubeconfig must be located in $HOME/.kube/config", EnvVarE2EKubeconfig))
+		}
+
+		kubeconfig = path.Join(homeDir, ".kube", "config")
+	}
+
+	e2eBaseDomain = os.Getenv(envVarBaseDomain)
+	if e2eBaseDomain == "" {
+		var restConfig *rest.Config
+		{
+			restConfig, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+			if err != nil {
+				panic(fmt.Sprintf("env var '%s' must not be empty, or correct kubeconfig must be specified, error %#v", envVarBaseDomain, err))
+			}
+		}
+
+		url, err := url.Parse(restConfig.Host)
+		if err != nil {
+			panic(fmt.Sprintf("env var '%s' must not be empty, or correct kubeconfig must be specified, error %#v", envVarBaseDomain, err))
+		}
+
+		// hostname has form "g8s.<installation name>.example.com"
+		hostname := url.Hostname()
+
+		// We need "k8s.<installation name>.example.com" for base domain
+		parts := strings.Split(hostname, ".")
+		parts[0] = "k8s"
+		e2eBaseDomain = strings.Join(parts, ".")
 	}
 }
 
