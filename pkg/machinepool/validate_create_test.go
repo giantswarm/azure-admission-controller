5aa08076018d07b34dd1647a0629621ecddd838f
Christian Bianchi <christian@giantswarm.io>
2021-11-25 12:56:52 +0100

    Disable az check (#339)
    * Remove failure domain check for `AzureMachine` because Azure credentials are unknown at creation time.
    
    * Remove failure domain check for `AzureMachine` because Azure credentials are unknown at creation time.

--- pkg/machinepool/validate_create_test.go
+++ pkg/machinepool/validate_create_test.go
@@ -35,42 +35,42 @@ func TestMachinePoolCreateValidate(t *testing.T) {
 	}
 
 	testCases := []testCase{
-		{
-			name:         "case 0: instance type supporting [1,2,3], requested [1]",
-			machinePool:  builder.BuildMachinePool(builder.AzureMachinePool(machinePoolName), builder.FailureDomains([]string{"1"})),
-			vmType:       "Standard_A2_v2",
-			errorMatcher: nil,
-		},
-		{
-			name:         "case 1: instance type supporting [1,2], requested [3]",
-			machinePool:  builder.BuildMachinePool(builder.AzureMachinePool(machinePoolName), builder.FailureDomains([]string{"3"})),
-			vmType:       "Standard_A4_v2",
-			errorMatcher: IsUnsupportedFailureDomainError,
-		},
-		{
-			name:         "case 2: instance type supporting [1,2], requested [2,3]",
-			machinePool:  builder.BuildMachinePool(builder.AzureMachinePool(machinePoolName), builder.FailureDomains([]string{"2,3"})),
-			vmType:       "Standard_A4_v2",
-			errorMatcher: IsUnsupportedFailureDomainError,
-		},
-		{
-			name:         "case 3: instance type supporting [], requested [1]",
-			machinePool:  builder.BuildMachinePool(builder.AzureMachinePool(machinePoolName), builder.FailureDomains([]string{"1"})),
-			vmType:       "Standard_A8_v2",
-			errorMatcher: IsUnsupportedFailureDomainError,
-		},
-		{
-			name:         "case 4: instance type supporting [], requested []",
-			machinePool:  builder.BuildMachinePool(builder.AzureMachinePool(machinePoolName), builder.FailureDomains([]string{})),
-			vmType:       "Standard_A8_v2",
-			errorMatcher: nil,
-		},
-		{
-			name:         "case 5: AzureMachinePool does not exist",
-			machinePool:  builder.BuildMachinePool(builder.AzureMachinePool(machinePoolName), builder.FailureDomains([]string{})),
-			vmType:       "",
-			errorMatcher: IsAzureMachinePoolNotFound,
-		},
+		//{
+		//	name:         "case 0: instance type supporting [1,2,3], requested [1]",
+		//	machinePool:  builder.BuildMachinePool(builder.AzureMachinePool(machinePoolName), builder.FailureDomains([]string{"1"})),
+		//	vmType:       "Standard_A2_v2",
+		//	errorMatcher: nil,
+		//},
+		//{
+		//	name:         "case 1: instance type supporting [1,2], requested [3]",
+		//	machinePool:  builder.BuildMachinePool(builder.AzureMachinePool(machinePoolName), builder.FailureDomains([]string{"3"})),
+		//	vmType:       "Standard_A4_v2",
+		//	errorMatcher: IsUnsupportedFailureDomainError,
+		//},
+		//{
+		//	name:         "case 2: instance type supporting [1,2], requested [2,3]",
+		//	machinePool:  builder.BuildMachinePool(builder.AzureMachinePool(machinePoolName), builder.FailureDomains([]string{"2,3"})),
+		//	vmType:       "Standard_A4_v2",
+		//	errorMatcher: IsUnsupportedFailureDomainError,
+		//},
+		//{
+		//	name:         "case 3: instance type supporting [], requested [1]",
+		//	machinePool:  builder.BuildMachinePool(builder.AzureMachinePool(machinePoolName), builder.FailureDomains([]string{"1"})),
+		//	vmType:       "Standard_A8_v2",
+		//	errorMatcher: IsUnsupportedFailureDomainError,
+		//},
+		//{
+		//	name:         "case 4: instance type supporting [], requested []",
+		//	machinePool:  builder.BuildMachinePool(builder.AzureMachinePool(machinePoolName), builder.FailureDomains([]string{})),
+		//	vmType:       "Standard_A8_v2",
+		//	errorMatcher: nil,
+		//},
+		//{
+		//	name:         "case 5: AzureMachinePool does not exist",
+		//	machinePool:  builder.BuildMachinePool(builder.AzureMachinePool("wrong"), builder.FailureDomains([]string{})),
+		//	vmType:       "",
+		//	errorMatcher: IsAzureMachinePoolNotFound,
+		//},
 		{
 			name:         "case 6: Wrong Organization",
 			machinePool:  builder.BuildMachinePool(builder.AzureMachinePool(machinePoolName), builder.Organization("wrongorg")),
